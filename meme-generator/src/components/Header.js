0d236658a566f45bc260051ecc23d5548f193cf1
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-03-28 16:57:56 +0100

    uploaded res for tutorial

---
+++ meme-generator/src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className='head'>
+            <img src="../public/troll-face.png" className='troll' />
+            Meme Generator
+        </header>
+    )
+}
