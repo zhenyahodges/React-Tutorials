66f8488a786b9e30c4cf4bf796607354fc910f2c
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-03-29 13:30:52 +0100

    added form data

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -2,30 +2,37 @@ import memesData from '../memesData';
 import { useState } from 'react';
 
 export default function Meme() {
-    // const [memeImage,setMemeImage]=useState("http://i.imgflip.com/1bij.jpg");
 
-    const [meme,setMeme]=useState({
-        topText:'',
-        bottomText:'',
-        randomImage:"http://i.imgflip.com/1bij.jpg"});   
-    // const meme={topText,bottomText,randomImage};
-    
-    const [allMemeImages,setAllMemeImages]=useState(memesData);
+    const [meme, setMeme] = useState({
+        first: '',
+        second: '',
+        randomImage: 'http://i.imgflip.com/1bij.jpg',
+    });
+
+    const [allMemeImages, setAllMemeImages] = useState(memesData);
 
     function getMemeImage() {
-        const memesArray= allMemeImages.data.memes;
-        const randomNum= Math.floor(Math.random()*memesArray.length);
-        const url=memesArray[randomNum].url;
-        
-        setMeme(prevMeme=>({
+        const memesArray = allMemeImages.data.memes;
+        const randomNum = Math.floor(Math.random() * memesArray.length);
+        const url = memesArray[randomNum].url;
+
+        setMeme((prevMeme) => ({
             ...prevMeme,
-            randomImage:url            
-        }));     
+            randomImage: url,
+        }));
     }
 
-    function hadnleMouseOverImg() {
-        // console.log('mouseover');
+
+    function handleChange(event) {
+        const {name, value} = event.target;
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }));
     }
+    // function hadnleMouseOverImg() {
+    //     // console.log('mouseover');
+    // }
 
     return (
         <main>
@@ -36,19 +43,29 @@ export default function Meme() {
                         className='form-input first'
                         type='text'
                         placeholder=' shut up'
+                        name="first"
+                        value={meme.first}
+                        onChange={handleChange}
                     ></input>
                     <input
                         id='second'
                         className='form-input second'
                         type='text'
                         placeholder=' and take my money'
+                        name="second"
+                    value={meme.second}
+                    onChange={handleChange}
                     ></input>
                 </div>
 
                 <button className='form-btn' onClick={getMemeImage}>
                     Get a new meme image
                 </button>
-                <img src={meme.randomImage} className='meme' onMouseOver={hadnleMouseOverImg}></img>
+                <div className='meme'>
+                    <img src={meme.randomImage} className='meme--image' />
+                    <h2 className='meme--text top'>{meme.first}</h2>
+                    <h2 className='meme--text bottom'>{meme.second}</h2>
+                </div>
             </div>
         </main>
     );
