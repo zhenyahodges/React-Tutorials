4b278f10da8a2674b40afff4e79ce3852131adda
ZHodges <114656373+zhenyahodges@users.noreply.github.com>
2023-03-30 22:36:40 +0100

    updates

--- tenzies-game/src/App.js
+++ tenzies-game/src/App.js
@@ -1,9 +1,32 @@
-import { Main } from "./components/Main";
+import { useState } from 'react';
+import Die from './components/Die';
 
 function App() {
-  return (  
+    const [dice, setDice] = useState(allNewDice());
 
-    <Main/>
+    function allNewDice() {
+        const newDice =[];
+              for (let i = 0; i < 10; i++) {
+           
+            newDice.push({
+                value: Math.ceil(Math.random() * 6),
+                isHeld:false
+            });
+        }
+        return newDice;
+    }
+
+    function rollDice() {
+        return setDice((s) => allNewDice());
+    }
+
+    const diceElements = dice.map((die) => <Die value={die.value} />);
+
+    return (
+        <main>
+            <div className='dice-container'>{diceElements}</div>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+        </main>
     );
 }
 
